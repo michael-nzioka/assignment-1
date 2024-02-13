0597c0cbdd90971afb045d68aa9583c4cefc943f
michael-nzioka <158498882+michael-nzioka@users.noreply.github.com>
2024-02-13 01:59:39 -0800

    main.c

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-    int a, b, c;
+    int a, b, c, d;
     printf("enter first value:");
     scanf("%d" , &a);
     printf("enter second value:");
     scanf("%d" , &b);
-    c = a + b;
-    printf("%d + %d = %d\n" , a, b, c);
+    printf("enter third value:");
+    scanf("%d" , &c);
+    d = a + b + c;
+    printf("%d + %d + %d = %d\n" , a, b, c, d);
     return 0;
 }
